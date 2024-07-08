cd6ad04271c5be679e50eced303ef7d0d38b5e58
Herlon Raphael <herlon.rapha@hotmail.com>
2024-07-07 23:36:02 -0300

    wipe teste camera

--- App.tsx
+++ App.tsx
@@ -1,11 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-reanimated'
+import { useEffect, useRef, useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Frame, useCameraDevice, useCameraDevices, useCameraPermission } from 'react-native-vision-camera';
+import {
+  Face,
+  Camera,
+  FaceDetectionOptions
+} from 'react-native-vision-camera-face-detector'
 
 export default function App() {
+  const [facing, setFacing] = useState('back');
+  const faceDetectionOptions = useRef<FaceDetectionOptions>( {
+    // detection options
+  } ).current
+
+  const device = useCameraDevice('front')
+  const { hasPermission, requestPermission } = useCameraPermission()
+  const [permission, setPermission] = useState<null | boolean>(null)
+
+  useEffect(() => {
+    (async () => {
+      const status = await requestPermission()
+      console.log('teste')
+      if (status) {
+        setPermission(true)
+      }
+    })()
+  }, [])
+
+  if (!permission || !device) {
+    return <View />
+  }
+
+
+  function handleFacesDetection(
+    faces: Face[],
+    frame: Frame
+  ) { 
+    console.log(
+      'faces', faces.length,
+      'frame', frame.toString()
+    )
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      {!!device? <Camera
+        isActive
+        style={StyleSheet.absoluteFill}
+        device={device}
+        faceDetectionCallback={ handleFacesDetection }
+        faceDetectionOptions={ faceDetectionOptions }
+      /> : <Text>
+        No Device
+      </Text>}
     </View>
   );
 }
@@ -13,8 +62,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+const path = require( "path" )
+const pak = require( "../package.json" )
+
+module.exports = {
+  presets: [ 'babel-preset-expo' ],
+  plugins: [ [
+    'module-resolver', {
+      alias: {
+        [ pak.name ]: path.join( __dirname, "..", pak.source )
+      },
+      root: [ './src' ],
+      'extensions': [
+        '.tsx',
+        '.ts',
+        '.js',
+        '.json'
+      ]
+    } ], [
+    'react-native-reanimated/plugin', {
+      processNestedWorklets: true
+    }
+  ], [
+    'react-native-worklets-core/plugin'
+  ] ]
+}
